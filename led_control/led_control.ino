8ab83858089afe5ecab40a9367c46659fa98b47c
Mathijs de Langen <langen@robot-rose.nl>
2015-04-21 00:09:34 +0200

    rewrote code

--- led_control/led_control.ino
+++ led_control/led_control.ino
@@ -6,23 +6,25 @@
 // Set pin numbers:
 const int LED_PIN            = 5;
 const int manual_control_pin = 6;  // The ID of the switch pin
-const int infrared_pin       = 7;  // The ID of the infrared sensor
+const int infrared_pin       = 3;  // The ID of the infrared sensor
 const int DELAY              = 100;
 const long ONE_SECOND        = (1000)/DELAY;
 const long ONE_MINUTE        = 60*ONE_SECOND;
 const long FIVE_MINUTES      = 5*ONE_MINUTE;
 const long TIMER_TIMEOUT     = 5*ONE_SECOND;
+
 enum state {
-  ON,
+  ON_BY_BUTTON,
+  ON_BY_INFRARED,
   OFF
 };
 
-int button_state             = 0;           // The initial state of the button
-int prev_button_state        = 0;
-int infrared_state           = 0;           // The initial state of the infrared sensor
-state current_led_state      = OFF;         // Current LED state
-state button_on              = OFF;
-long led_on_timer            = 0;           // Timer to count how long the LED is on
+int   button_state           = 0;           // The initial state of the button
+int   prev_button_state      = 0;           // Store the previous button state
+int   infrared_state         = 0;           // The initial state of the infrared sensor
+state led_state              = OFF;         // Current LED state
+state prev_led_state         = OFF;
+long  led_on_timer           = 0;           // Timer to count how long the LED is on
 
 CRGB leds[NUM_LEDS];                        // Led array
 
@@ -42,90 +44,87 @@ void setup()
 void loop()
 {
   // Read button value
-  button_state   = digitalRead(manual_control_pin);
-  infrared_state = digitalRead(infrared_pin);
-  
-  // Button pressed
-  if ( button_state == HIGH && button_state != prev_button_state)
-  {
-    switchMode();
-  }
-  
-  if ( infrared_state == HIGH )
-  {
-    Serial.println("I saw something");
-    switchOn();
-  }
-  
-  ledTimer();
-  
   prev_button_state = button_state;
+  button_state      = digitalRead(manual_control_pin);
+  infrared_state    = digitalRead(infrared_pin);
+  
+  if ( prev_button_state != button_state )
+    determineNewState(button_state, infrared_state);
+  else
+    determineNewState(0, infrared_state);
+    
+  checkTimeouts();
+  executeLedState();
+  
+  //Serial.println("I saw something");
   
   delay(DELAY);
 }
 
-void ledTimer()
-{ 
-  // Button override
-  if ( button_on == ON )
-    return;
-   
-  if ( current_led_state == ON )
+void determineNewState(int button_state, int infrared_state)
+{
+  Serial.print("Button state:");
+  Serial.print(button_state);
+  Serial.print(", Infrared state:");
+  Serial.println(infrared_state);
+  
+  Serial.print("Prev state:");
+  Serial.println(prev_led_state);
+
+  prev_led_state = led_state;
+  
+  switch (prev_led_state)
   {
-    if ( led_on_timer == 0 )
-    {
-      allLedsWhite();
-    }
-    Serial.println("Increasing timer");
-    Serial.println(led_on_timer);
-    led_on_timer++;
+    case OFF:
+      if ( button_state == HIGH )
+        led_state = ON_BY_BUTTON;
+      else if ( infrared_state == HIGH )
+        led_state = ON_BY_INFRARED;
+    break;
+    case ON_BY_BUTTON:
+      if ( button_state == HIGH )
+        led_state = OFF;
+    break;
+    case ON_BY_INFRARED:
+      if ( button_state == HIGH )
+        led_state = OFF;
+      else if ( infrared_state == HIGH )
+        led_on_timer = 0;
+    break;
   }
   
-  if ( current_led_state == OFF )
-  {
-    if ( led_on_timer > 0 )
-    {
-       allLedsOff();
-    }
-    led_on_timer = 0;
-    return;
-  }
+  Serial.print("New state:");
+  Serial.println(led_state);
   
-  Serial.println(led_on_timer);
+}
+
+void checkTimeouts()
+{
+  Serial.print("Timer:");
+  Serial.print(led_on_timer);
+  Serial.print("/");
   Serial.println(TIMER_TIMEOUT);
- 
-  // Check for exceeded time
+  
   if ( led_on_timer > TIMER_TIMEOUT )
   {
-    switchOff();
+    led_state    = OFF;
+    led_on_timer = 0;
   }
 }
 
-void switchMode()
+void executeLedState()
 {
-  if ( button_on == OFF )
-  {
-    button_on = ON;
-    allLedsWhite();
-  }
-  else
-  {
-    button_on = OFF;
-    allLedsOff(); 
-  }
+  // Something changed
+  if ( prev_led_state != led_state )
+     if ( led_state == OFF )
+       allLedsOff();
+     else
+       allLedsWhite();
+  else // The same state
+    if ( led_state == ON_BY_INFRARED )
+      led_on_timer++;
 }
 
-void switchOn()
-{
-  // Reset the timer
-  led_on_timer      = 0;
-  current_led_state = ON;
-}
-
-void switchOff()
-{
-  current_led_state = OFF;
-}
 
 void allLedsWhite()
 {
